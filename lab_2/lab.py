0fa0e39f7935454bc8a4d5aa8225c8d0442c3f6d
TimeBean <alexanderklush8@gmail.com>
2026-09-26 12:29:52 +0300

    fix: move misplaced lab 2 to lab_2 directory

--- lab_2/lab.py
+++ lab_2/lab.py
@@ -4,11 +4,11 @@ from scipy.integrate import odeint  # Ordinary Differential Equation Integrator
 
 volume = 52  # Объем бака
 theta_1 = 15  # Температура первого потока (холодная вода)
-theta_2 = 55  # Температура второго потока (горячая вода)
+theta_2 = 65  # Температура второго потока (горячая вода)
 g_1 = 4  # Расход первого потока (холодная вода)
 g_2 = 3  # Расход второго потока (горячая вода)
 
-T0 = 25
+theta_0 = 25
 
 def heat_balance_ode(T, t):
     dT_dt = (g_1 * theta_1 + g_2 * theta_2 - (g_1 + g_2) * T) / volume
@@ -16,7 +16,7 @@ def heat_balance_ode(T, t):
 
 t = numpy.linspace(0, 60, 600)
 
-T_solution = odeint(heat_balance_ode, T0, t)
+T_solution = odeint(heat_balance_ode, theta_0, t)
 
 T_steady = (g_1 * theta_1 + g_2 * theta_2) / (g_1 + g_2)  # dT_dt = 0
 print(f"Установившаяся температура: {T_steady:.2f} градусов")
@@ -34,5 +34,5 @@ plot.legend()
 plot.grid(True)
 
 file_name = 'simulation.png'
-plot.savefig(file_name, dpi=100)
+plot.savefig(file_name, dpi=150)
 print(f"График сохранен в файл '{file_name}'")
